80709e86eeaa47c82795bc0e3167b1bd86bd087d
dmod <adarmody@gmail.com>
2018-07-25 22:40:31 -0400

    Adding unit test project

---
+++ SudokuSolverTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SudokuSolverTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SudokuSolverTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd8374e7-30d4-4f16-b951-279c12b1c50d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SudokuSolverTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SudokuSolverTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] array5 = new int[,]
+            {
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            };
+        }
+
+        [TestMethod]
+        public void TestTopLeftLogic()
+        {
+            SudokuSolver.Game.GetTopLeftOf3x3Square(0, 0, out int rowOut, out int colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(0, 1, out rowOut, out colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(1, 0, out rowOut, out colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(1, 1, out rowOut, out colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(2, 2, out rowOut, out colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(1, 2, out rowOut, out colOut);
+            Assert.AreEqual(0, rowOut);
+            Assert.AreEqual(0, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(5, 5, out rowOut, out colOut);
+            Assert.AreEqual(3, rowOut);
+            Assert.AreEqual(3, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(7, 8, out rowOut, out colOut);
+            Assert.AreEqual(6, rowOut);
+            Assert.AreEqual(6, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(8, 8, out rowOut, out colOut);
+            Assert.AreEqual(6, rowOut);
+            Assert.AreEqual(6, colOut);
+
+            SudokuSolver.Game.GetTopLeftOf3x3Square(7, 7, out rowOut, out colOut);
+            Assert.AreEqual(6, rowOut);
+            Assert.AreEqual(6, colOut);
+        }
+    }
+}
